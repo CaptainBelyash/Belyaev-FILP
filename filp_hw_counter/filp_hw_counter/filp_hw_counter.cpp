ee6a819eedd6937398150439a1265990d1a52f96
CaptainBelyash <50026308+CaptainBelyash@users.noreply.github.com>
2020-03-08 18:25:35 +0500

    git organize

--- filp_hw_counter/filp_hw_counter/filp_hw_counter.cpp
+++ filp_hw_counter/filp_hw_counter/filp_hw_counter.cpp
@@ -1,6 +1,38 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int fileSize(char fileName[]) {
+	ifstream file(fileName, ios::binary | ios::ate);
+	int lenght = int(file.tellg());
+	file.close();
+	return lenght;
+}
+
+char getDataFromFile(char fileName[]) {
+
+}
+
+void myFunc(char bookName[]) {
+	int lenght = fileSize(bookName);
+	cout << lenght << endl;
+	ifstream bookFile;
+	bookFile.open(bookName);
+	char readerBuffer[50];
+	int n;
+	cout << "Say a number:" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		bookFile >> readerBuffer;
+		cout << readerBuffer << " ";
+	}
+	bookFile.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char bookName[] = "book.txt";
+	myFunc(bookName);
 }
