ca60668db395a5739dcd34c130cb6d054f5e341e
CaptainBelyash <50026308+CaptainBelyash@users.noreply.github.com>
2020-06-06 17:05:06 +0500

    Task 3 done
    Task1 and Task3 done

--- smart_pointer/smart_pointer/smart_pointer/smart_pointer.cpp
+++ smart_pointer/smart_pointer/smart_pointer/smart_pointer.cpp
@@ -2,8 +2,31 @@
 #include <iostream>
 #include "smart_pointer.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 
+
+
+int main() {
+	char* ptr = new char(97);
+
+	auto* first = new SmartPointer<char>(ptr);
+
+	std::cout << "First get: " << first->Get()
+		<< "; count:" << *(first->ref_count) << std::endl;
+	
+	auto* second = first;
+	std::cout << "First get: " << first->Get()
+		<< "; count:" << *(first->ref_count) << std::endl;
+	std::cout << "Second get: " << second->Get()
+		<< "; count:" << *(second->ref_count) << std::endl;
+	ptr = new char(98);
+	second->Set(ptr);
+	std::cout << "First get: " << first->Get()
+		<< "; count:" << *(first->ref_count) << std::endl;
+	std::cout << "Second get: " << second->Get()
+		<< "; count:" << *(second->ref_count) << std::endl;
+	first->Release();
+	std::cout << "First get: " << first->Get()
+		<< "; count:" << *(first->ref_count) << std::endl;
+	std::cout << "Second get: " << second->Get()
+		<< "; count:" << *(second->ref_count) << std::endl;
+}
---
+++ smart_pointer/smart_pointer/smart_pointer/smart_pointer.h
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SmartPointer
+{
+public:
+	T* ref;
+	unsigned* ref_count;
+
+	SmartPointer(T* ptr) {
+		ref = ptr;
+		ref_count = (unsigned*)malloc(sizeof(unsigned));
+		*ref_count = 1;
+	}
+
+	T Get() {
+		return *ref;
+	}
+	
+	void Set(T* newRef) {
+		ref = newRef;
+		(*ref_count)++;
+	}
+
+	void Release() {
+		(*ref_count)--;
+		if (*ref_count == 0) {
+			free(ref_count);
+			delete ref;
+
+			ref_count = nullptr;
+			ref = nullptr;
+		}
+			
+	}
+
+	SmartPointer& operator=(SmartPointer& newPointer) {
+		if (*ref_count > 0) {
+			Release();
+		}
+
+		if (this != &newPointer) {
+			ref = newPointer.ref;
+			ref_count = newPointer.ref_count;
+			(*ref_count)++;
+		}
+		return *this;
+	}
+
+	T operator->() {
+		return ref;
+	}
+	
+	T& operator*() {
+		return *ref;
+	}
+};
